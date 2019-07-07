88bbab1bd39a9aebf9c24d160914f79ffbc84df7
devnikop <petrov.petrov.ak@gmail.com>
2019-07-07 19:09:07 +0300

    add buttons in info page

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {
   Text,
   View,
+  Button,
 } from 'react-native'
 import {
   NavigationScreenProp,
@@ -22,6 +23,13 @@ interface NavigationParams {
 type Navigation = NavigationScreenProp<NavigationState, NavigationParams>
 
 class Info extends React.PureComponent<Props> {
+  constructor(props) {
+    super(props)
+
+    this._handlerBackButtonPress = this._handlerBackButtonPress.bind(this)
+    this._handlerSignOffPress = this._handlerSignOffPress.bind(this)
+  }
+
   render() {
     const {
       navigation
@@ -36,8 +44,24 @@ class Info extends React.PureComponent<Props> {
       <View style={style.info}>
         <Text style={style.infoTitle}>{title}</Text>
         <Text style={style.infoDescription}>{description}</Text>
+        <Button
+          onPress={this._handlerBackButtonPress}
+          title={`Назад`}
+        />
+        <Button
+          onPress={this._handlerSignOffPress}
+          title={`Выйти из аккаунта`}
+        />
       </View>
     )
+  }
+
+  _handlerBackButtonPress() {
+    this.props.navigation.goBack()
+  }
+
+  _handlerSignOffPress() {
+    this.props.navigation.navigate("SignIn")
   }
 
   static navigationOptions = {
